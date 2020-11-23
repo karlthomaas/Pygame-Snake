c58712c197cef729a24d0dbbb7a4627127541a6a
karlthomaas <73159568+karlthomaas@users.noreply.github.com>
2020-11-23 15:55:40 +0200

    Enemy movement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # without this, the game wouldn't work
 pygame.init()
@@ -15,20 +16,37 @@ pygame.display.set_icon(icon)
 
 # Player
 playerImg = pygame.image.load('space-ship.png')
+# Player spawn location
 playerX = 370
 playerY = 480
 
 playerX_change = 0
 playerY_change = 0
-playerXY_up_change = 0
-playerXY_down_change = 0
+
 
 # player 1 function
 def player(x, y):
     # blit, means draw
     # basically you draw the playerimg to a certain position
-
     screen.blit(playerImg, (x, y))
+
+
+# Enemy
+enemyImg = pygame.image.load('red-enemy.png')  # enemy picture
+
+# Enemy spawn location
+# random.randint(randomises the enemy spawn location)
+enemyX = random.randint(1, 800)
+enemyY = random.randint(1, 100)
+
+# Enemy movement
+enemyX_Change = 0.3
+enemyY_Change = 40
+
+
+# Enemy function
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 
 # game loop
@@ -66,7 +84,6 @@ while running:
             if event.key == pygame.K_DOWN:
                 playerY_change = 0.1
 
-
             if event.key == pygame.K_SPACE:
                 print('piu')
 
@@ -81,10 +98,32 @@ while running:
                 playerY_change = 0
 
 
-    # puts the player onto window
     playerX += playerX_change
     playerY += playerY_change
+
+    # if player cords is less than zero (borderline), it changes the location to the other side
+    if playerX <= 0:
+        playerX = 750
+    # if player cords is more than borderline, it changes the player location to the other side
+    elif playerX >= 750:
+        playerX = 0
+
+    # firstly moves to the right
+    enemyX += enemyX_Change
+
+    # if enemy touches border, it goes the other way
+    if enemyX >= 780:
+        enemyX_Change = -0.3
+        enemyY += enemyY_Change
+
+
+
+    elif enemyX <= 0:
+        enemyX_Change = 0.3
+        enemyY += enemyY_Change
+
     player(playerX, playerY)
+    enemy(enemyX, enemyY)
 
     # updates display, for new movements etc..
     pygame.display.update()
